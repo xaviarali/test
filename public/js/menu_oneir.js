1b169c883fc7742f79f6b32a22837e940e3fda1e
xaviarali <xaviar4>
2016-09-05 20:24:41 +0500

    Changes

--- public/js/menu_oneir.js
+++ public/js/menu_oneir.js
@@ -354,7 +354,7 @@ function eventListenersForAllLevels(code)
 							var level1 = (g+1), level2 = (i+1);   
 				            if( (g+1) > 9 ) m1 = telnetCorrespondence[(g+1)%10];
 							if( (i+1) > 9 ) m1 = telnetCorrespondence[(i+1)%10];
-							bindEventListeners('#'+code[g]+(i+1),level1+','+level2+','+sessionId','+code[g]);
+							bindEventListeners('#'+code[g]+(i+1),level1+','+level2+','+sessionId+','+code[g]);
 			        }
 	             }
 	        }
